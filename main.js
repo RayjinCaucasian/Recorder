3cf280696a14a27b839a937462445b7e0e9fe9c9
Chad Blevins <chad.d.blevins@gmail.com>
2021-09-07 10:47:29 -0700

    redoing js file- honeslty not sute where im going with this.

--- main.js
+++ main.js
@@ -1,82 +1,76 @@
-class Media {
-    constructor() {
-        this._ctx = new AudioContext({
-            sampleRate: 48000,
-            latencyHint: "interactive",
-        });
+const ctx = new AudioContext({ latencyHint: "interactive", sampleRate: 48000 });
+let stream;
+let recorder;
+const files = [];
 
-        this._stream;
-        this._recorder;
-        this._micNode;
-        this._fileSrc;
-        this._files = [];
-    }
-    async getStream() {
-        if (!this._stream) {
-            try {
-                this._stream = await navigator.mediaDevices.getUserMedia({
-                    audio: true,
-                });
-            } catch (e) {
-                throw e;
-            }
-        }
-        return this._stream;
-    }
-    async initRecorder() {
-        let chunks = [];
-        if (!this._recorder) {
-            try {
-                this._recorder = new MediaRecorder(await this.getStream());
-                this._recorder.ondataavailable = (e) => chunks.push(e.data);
-                this._recorder.onstop = () => {
-                    const blob = new Blob(chunks);
-                    this._files.push(blob);
-                    chunks = [];
-                };
-            } catch (e) {
-                throw e;
-            }
+//init MediaStream
+async function initStream() {
+    if (!this._stream) {
+        try {
+            this._stream = await navigator.mediaDevices.getUserMedia({
+                audio: true,
+            });
+        } catch (e) {
+            throw e;
         }
     }
-    async record() {
-        if (!this._recorder) {
-            try {
-                await this.initRecorder();
-            } catch (e) {
-                alert(`Access to microphone failed: ${e.message}`);
-                console.log(e)
-                return;
-            }
+    return this._stream;
+}
+//init MediaRecorder with Stream dependecy if needed
+async function initRecorder() {
+    let blob = {};
+    if (!this._recorder) {
+        try {
+            recorder = new MediaRecorder(await this.initStream());
+        } catch (e) {
+            throw e;
         }
-        if (this._recorder.state === "inactive") {
-            this._recorder.start();
-        } else {
-            this._recorder.stop();
-        }
+        //Recorder Events
+        recorder.ondataavailable = (e) => {
+            blob = e.data;
+        };
+        recorder.onstop = () => {
+            files.push(new AudioFile("hello", blob, Date.now()));
+            blob = {};
+        };
     }
 }
 
-//MEDIA
-const media = new Media();
-const recordBtn = document
-    .getElementById("record-btn")
-    .addEventListener("click", onRecord);
+class AudioFile {
+    constructor(name, data, createdOn) {
+        this._name = name;
+        this._data = data;
+        this._createdOn = createdOn;
+    }
+}
 
-function onRecord() {
-    media.record();
-    console.log(media);
+//Record
+const recordBtn = document.getElementById("record-btn");
+recordBtn.addEventListener("click", onRecord);
+
+async function onRecord() {
+    if (!recorder) {
+        try {
+            await initRecorder();
+        } catch (e) {
+            alert(`Access to microphone stopped: ${e.message}`);
+        }
+    }
+    if (recorder.state === "inactive") {
+        recorder.start();
+        recordBtn.style.backgroundColor = "red";
+    }else{
+        recorder.stop();
+        recordBtn.style.backgroundColor = "";
+    }
 }
 
 //file selection
-const fileList = document.getElementById("file-list");
 document.addEventListener("click", onFileSelect);
 
 function onFileSelect(event) {
     let target = event.target;
     if (target.parentNode.className === "file" && target.nodeName === "LABEL") {
         event.preventDefault();
-        target.classList.toggle("selected");
     }
-    console.log();
 }
